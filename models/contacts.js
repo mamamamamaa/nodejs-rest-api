9081ccc6e2ef49517cd56b44e6d4907f3de62313
mamamamamaa <maks.denishchuk@gmail.com>
2022-12-18 17:16:02 +0200

    ///

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,96 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const dataFile = "models/contacts.json";
 
-const getContactById = async (contactId) => {}
+const updateDataBase = async (updatedContacts) => {
+  const jsonData = JSON.stringify(updatedContacts);
+  try {
+    await fs.writeFile(dataFile, jsonData);
+    return true;
+  } catch (e) {
+    return new Error(e.message);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(dataFile);
+    return JSON.parse(data.toString());
+  } catch (e) {
+    return new Error(e.message);
+  }
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const id = contactId.toString();
+    const [contactById] = contacts.filter((contact) => contact.id === id);
+    return contactById;
+  } catch (e) {
+    return new Error(e.message);
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const id = contactId.toString();
+
+    const newContacts = contacts.filter((contact) => contact.id !== id);
+
+    if (JSON.stringify(contacts) !== JSON.stringify(newContacts)) {
+      await updateDataBase(newContacts);
+    }
+
+    return newContacts;
+  } catch (e) {
+    return new Error(e.message);
+  }
+};
+
+const addContact = async (body) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = { ...body, id: nanoid() };
+
+    contacts.push(newContact);
+
+    await updateDataBase(contacts);
+    return newContact;
+  } catch (e) {
+    return new Error(e.message);
+  }
+};
+
+const updateContact = async (contactId, body = {}) => {
+  // const isEmpty = Object.keys(body).length === 0 && body.constructor === Object;
+  //
+  // if (isEmpty) {
+  //   return "Not found";
+  // }
+
+  try {
+    const contacts = await listContacts();
+    const id = contactId.toString();
+    let updatedContact = {};
+
+    const newContacts = contacts.map((contact) => {
+      if (id === contact.id) {
+        updatedContact = { ...contact, ...body };
+        return updatedContact;
+      }
+      return contact;
+    });
+
+    await updateDataBase(newContacts);
+
+    return updatedContact;
+  } catch (e) {
+    return new Error(e.message);
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +98,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,108 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const Joi = require("joi");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  addContact,
+  updateContact,
+  getContactById,
+  removeContact,
+  listContacts,
+} = require("../../models/contacts.js");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const addContactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string()
+    .pattern(
+      // eslint-disable-next-line prefer-regex-literals
+      new RegExp("^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$")
+    )
+    .required(),
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string().pattern(
+    // eslint-disable-next-line prefer-regex-literals
+    new RegExp("^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$")
+  ),
+}).required();
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const data = await listContacts();
 
-module.exports = router
+  const response = data
+    ? { data, message: "Success", status: 200 }
+    : { message: "Server problem", status: 500 };
+
+  res.json(response);
+});
+
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+
+  const data = await getContactById(contactId);
+
+  const response = data
+    ? { data, message: "Success", status: 200 }
+    : { message: "Not found", status: 404 };
+
+  res.json(response);
+});
+
+router.post("/", async (req, res, next) => {
+  const { error, value } = addContactSchema.validate(req.body);
+
+  if (error) {
+    res.json({ message: "Missing required name field", status: 404 });
+    return;
+  }
+
+  const data = await addContact(value);
+
+  const response = data
+    ? { data, message: "Success", status: 200 }
+    : { message: "Server problem", status: 500 };
+
+  res.json(response);
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const data = await removeContact(contactId);
+
+  const response = data
+    ? { data, message: "Success", status: 200 }
+    : { message: "Server problem", status: 500 };
+
+  res.json(response);
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const { error, value } = updateContactSchema.validate(req.body);
+
+  if (error) {
+    res.json({ message: "Should be at least one of fields", status: 404 });
+    return;
+  }
+
+  const data = await updateContact(contactId, value);
+
+  const response = data
+    ? { data, message: "Success", status: 200 }
+    : { message: "Server problem", status: 500 };
+
+  res.json(response);
+});
+
+module.exports = router;
