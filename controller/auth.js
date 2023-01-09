5cec387377e1bee8621b8d082e356c9f75fd61bf
mamamamamaa <maks.denishchuk@gmail.com>
2023-01-09 13:42:51 +0200

    add lopin part with jwt

--- controller/auth.js
+++ controller/auth.js
@@ -1,6 +1,9 @@
 const User = require("../models/user");
-const { loginSchema, registerSchema } = require("../schemas/schemas");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { loginSchema, registerSchema } = require("../schemas/schemas");
+const { SECRET_KEY } = process.env;
 
 const registration = async (req, res) => {
   try {
@@ -16,7 +19,7 @@ const registration = async (req, res) => {
 
     const { email, password } = value;
 
-    const user = User.findOne(email);
+    const user = User.findOne({ email });
 
     if (user) {
       res.json({ message: "Email in use", status: 409 });
@@ -37,6 +40,52 @@ const registration = async (req, res) => {
   }
 };
 
-const login = async (req, res) => {};
+const login = async (req, res) => {
+  try {
+    const { error, value } = loginSchema.validate(req.body);
+
+    if (error) {
+      res.json({
+        message: "Invalid values",
+        status: 404,
+      });
+      return;
+    }
+
+    const { email, password } = value;
+
+    const user = User.findOne({ email });
+
+    if (!user) {
+      res.json({ message: "Invalid email or password", status: 409 });
+      return;
+    }
+
+    const comparePassword = await bcrypt.compare(password, user.password);
+
+    if (!comparePassword) {
+      res.json({ message: "Invalid email or password", status: 409 });
+      return;
+    }
+
+    const payload = {
+      id: user._id,
+    };
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "8h" });
+
+    res.json({
+      message: "Success",
+      status: 201,
+      data: {
+        token,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (e) {
+    res.json({ message: "Server error", status: 500 });
+  }
+};
 
 module.exports = { login, registration };
