6e7bc70560c8a31dfe34d4e47e3fe2b8c9f72864
mamamamamaa <maks.denishchuk@gmail.com>
2023-01-10 09:58:08 +0200

    //

--- controller/contact.js
+++ controller/contact.js
@@ -9,7 +9,7 @@ const HttpError = require("../middlewares/HttpError");
 
 const listContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10, favorite =  } = req.query;
+  const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
   try {
     const contacts = await Contact.find({ owner }, "-createdAt -updatedAt", {
