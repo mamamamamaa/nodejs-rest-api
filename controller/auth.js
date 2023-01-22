e0f7de3854c70818961c16592134339242a7cff8
mamamamamaa <maks.denishchuk@gmail.com>
2023-01-22 17:45:22 +0200

    //

--- controller/auth.js
+++ controller/auth.js
@@ -1,16 +1,23 @@
 const User = require("../models/user");
+const Jimp = require("jimp");
+const HttpError = require("../middlewares/HttpError");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const fs = require("fs").promises;
+const path = require("path");
 const gravatar = require("gravatar");
 const { nanoid } = require("nanoid");
+const sgMail = require("@sendgrid/mail");
 
 const {
   loginSchema,
   registerSchema,
   subscriptionSchema,
 } = require("../schemas/schemas");
-const HttpError = require("../middlewares/HttpError");
-const { SECRET_KEY } = process.env;
+
+const { SECRET_KEY, SENDGRID_API_KEY, MY_EMAIL, BASE_URL } = process.env;
+
+sgMail.setApiKey(SENDGRID_API_KEY);
 
 const registration = async (req, res, next) => {
   try {
@@ -30,6 +37,16 @@ const registration = async (req, res, next) => {
     const hashPassword = await bcrypt.hashSync(password, 10);
     const avatarURL = gravatar.url(email);
     const verificationToken = nanoid();
+
+    const verifyMessage = {
+      to: email,
+      from: MY_EMAIL,
+      subject: "Verification code",
+      text: "To verify your account you should click on link bellow",
+      html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click verify email</a>`,
+    };
+
+    await sgMail.send(verifyMessage);
 
     const newUser = await User.create({
       ...req.body,
@@ -126,10 +143,56 @@ const logout = async (req, res) => {
   res.status(201).json({ message: "Logout success" });
 };
 
+const uploadPath = path.join(__dirname, "../", "public", "avatars");
+
+const changeAvatar = async (req, res, next) => {
+  const { _id } = req.user;
+  const { path: temporaryName, originalname } = req.file;
+  await Jimp.read(temporaryName)
+    .then((avatar) => {
+      return avatar.resize(250, 250).write(temporaryName);
+    })
+    .catch((err) => next(409, err));
+
+  const fileName = `${_id}_${originalname}`;
+  const pathToResizedAvatar = path.join(uploadPath, fileName);
+
+  await fs.rename(temporaryName, pathToResizedAvatar);
+
+  const avatarURL = path.join("avatar", fileName);
+
+  try {
+    await User.findByIdAndUpdate(_id, { avatarURL });
+  } catch {
+    next(HttpError(500));
+  }
+
+  res.status(201).json({ avatarURL });
+};
+
+const verify = async (req, res, next) => {
+  const { verificationToken } = req.params;
+
+  const user = await User.findOne({ verificationToken });
+
+  if (!user) {
+    next(HttpError(404, "User not found"));
+  }
+
+  await User.findByIdAndUpdate(user._id, {
+    verificationToken: null,
+    verify: true,
+  });
+
+  res.status(200).json({ message: "Verification successful" });
+};
+
 module.exports = {
   login,
   registration,
   getCurrent,
   logout,
   changeSubscription,
+  changeAvatar,
+  verify,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,18 +4,24 @@ const router = express.Router();
 
 const authenticate = require("../../middlewares/authenticate");
 
+const { upload } = require("../../middlewares/multer");
 const {
   login,
   registration,
   changeSubscription,
   getCurrent,
   logout,
+  changeAvatar,
+  verify,
 } = require("../../controller/auth");
 
 router.post("/login", login);
 router.post("/signup", registration);
+
 router.patch("/users", authenticate, changeSubscription);
 router.get("/current", authenticate, getCurrent);
 router.get("/logout", authenticate, logout);
+router.patch("/avatars", authenticate, upload.single("avatar"), changeAvatar);
+router.get("/verify/:verificationToken", verify);
 
 module.exports = router;
